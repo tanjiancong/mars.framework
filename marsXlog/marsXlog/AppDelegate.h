c1785da502dd4bf7774f403d1cb205fd93ceb61e
tanjiancong <tanjiancong@meiqijiacheng.com>
2023-02-02 17:19:21 +0800

    first commit

---
+++ marsXlog/marsXlog/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  marsXlog
+//
+//  Created by jc on 2023/2/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ marsXlog/marsXlog/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  marsXlog
+//
+//  Created by jc on 2023/2/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
